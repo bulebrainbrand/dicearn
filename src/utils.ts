442f3ac808d4c95eab841afc8a938c7bdfb5adcf
bulebrainbrand <h.yotsumoto0509@gmail.com>
2026-08-07 18:53:50 +0900

    refactor: delete unnesery variable

--- src/utils.ts
+++ src/utils.ts
@@ -1,16 +1,3 @@
-import { CELL_SIZE_PX } from "./constants.ts";
-import { Route } from "./board/BoardViewCoordinateCalculator.ts";
-
-export const modulo = (n: number, d: number) => ((n % d) + d) % d;
-
-export const routeToPxPos = (
-  pos: Route,
-): { x: number; y: number; type: "set" | "move" } => ({
-  x: pos.x * CELL_SIZE_PX,
-  y: pos.y * CELL_SIZE_PX,
-  type: pos.type,
-});
-
 export const times = (func: Function, times: number) => {
   for (let i = 0; i < times; i++) func();
 };
