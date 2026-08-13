4adbc4844c43538119f7ce9860bd1d4c18f934fa
bulebrainbrand <h.yotsumoto0509@gmail.com>
2026-08-13 21:33:06 +0900

    feat: create rewordChoiceModel

--- src/RewordChoice/Model.ts
+++ src/RewordChoice/Model.ts
@@ -1,5 +1,5 @@
 import EventEmitter from "phaser4-rex-plugins/plugins/utils/eventemitter/EventEmitter";
-import { TODO } from "untodo";
+
 export type Reword = { name: string; desc: string; callback: CallableFunction };
 export type Rewords = [
   Reword | undefined,
@@ -21,9 +21,19 @@ export class RewordChoice extends EventEmitter {
     this.status = { type: "hidden" };
   }
   public choice(index: 0 | 1 | 2): void {
-    TODO({ reason: "選ぶかんじのやつ" });
+    if (this.status.type === "hidden")
+      throw new TypeError(`can't choice when reword is hidden`);
+    this.emit("choice", {
+      index,
+      rewords: this.status.rewords,
+    } satisfies RewordChoiceModelEvent["choice"]);
   }
   public show(rewords: Rewords) {
-    TODO({ reason: "有効にする感じ" });
+    this.status = { rewords, type: "shown" };
+    this.emit("show", { rewords } satisfies RewordChoiceModelEvent["show"]);
+  }
+  public hide() {
+    this.status = { type: "hidden" };
+    this.emit("hide");
   }
 }
