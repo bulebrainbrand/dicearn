30c52af2ecfebc55359e05659c5acbc92fe2b18d
bulebrainbrand <h.yotsumoto0509@gmail.com>
2026-08-13 21:29:50 +0900

    feat: create reword generator

--- src/RewordChoice/RewordGenerator.ts
+++ src/RewordChoice/RewordGenerator.ts
@@ -1,8 +1,35 @@
-import { TODO } from "untodo";
 import { Rewords } from "./Model";
+import { InventoryModel } from "@/inventory/Model";
+import { BoardSize } from "@/types";
 
 export class RewordGenerator {
+  constructor(
+    private readonly inventoryModel: InventoryModel,
+    private readonly boardSize: BoardSize,
+  ) {}
   generate(): Rewords {
-    TODO({ reason: "後回し" });
+    return [
+      {
+        name: "normal tile",
+        desc: "get 1 normal tile",
+        callback: () => {
+          this.inventoryModel.addTile("normal", 1);
+        },
+      },
+      {
+        name: "buffer tile",
+        desc: "get 1 buffer tile",
+        callback: () => {
+          this.inventoryModel.addTile("buffer", 1);
+        },
+      },
+      {
+        name: "upgrade board",
+        desc: "board size +1",
+        callback: () => {
+          this.boardSize.expand(1);
+        },
+      },
+    ];
   }
 }
