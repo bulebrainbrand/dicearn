57ca41eb28301d5333f648f285c057faa565938a
bulebrainbrand <h.yotsumoto0509@gmail.com>
2026-08-17 12:50:47 +0900

    fix: add subrange

--- src/layer.ts
+++ src/layer.ts
@@ -1,3 +1,6 @@
+/**
+ * 開区間
+ */
 export class DepthRange {
   constructor(
     readonly min: number,
@@ -18,14 +21,29 @@ export class DepthRange {
       );
     return this.min + offset;
   }
+  subRange(min: number, max: number) {
+    const range = this.max - this.min;
+    if (max < min)
+      throw new RangeError(
+        `min (${min}) must not be greater than max (${max})`,
+      );
+    if (min < 0 || range < min)
+      throw new RangeError(
+        `invalid min: ${min}. it should be 0 =< min =< range(${range})`,
+      );
+    if (max < 0 || range < max)
+      throw new RangeError(
+        `invalid max: ${max}. it should be 0 =< max =< range(${range})`,
+      );
+    return new DepthRange(this.min + min, this.max + max);
+  }
 }
 
-export const UI_DEPTH_RANGE = new DepthRange(201, 210);
-
+export const UI_DEPTH_RANGE = new DepthRange(201, 250);
 export const BOARD_DEPTH_RANGE = new DepthRange(0, 10);
 export const CURSOR_DEPTH_RANGE = new DepthRange(11, 20);
-export const DICE_DEPTH_RANGE = new DepthRange(211, 220);
-export const MONEY_DEPTH_RANGE = new DepthRange(221, 230);
-export const INVENTORY_DEPTH_RANGE = new DepthRange(231, 240);
+export const DICE_DEPTH_RANGE = UI_DEPTH_RANGE.subRange(10, 20);
+export const MONEY_DEPTH_RANGE = UI_DEPTH_RANGE.subRange(21, 30);
+export const INVENTORY_DEPTH_RANGE = new DepthRange(191, 200);
 export const DRAGGING_DEPTH_RANGE = new DepthRange(151, 160);
-export const REWORDS_DEPTH_RANGE = new DepthRange(241, 250);
+export const REWORDS_DEPTH_RANGE = UI_DEPTH_RANGE.subRange(31, 40);
