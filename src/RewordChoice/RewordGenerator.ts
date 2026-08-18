f81260646326724c585043a16ff821b8d6e530be
bulebrainbrand <h.yotsumoto0509@gmail.com>
2026-08-18 14:13:38 +0900

    feat: update rewordGenerator

--- src/RewordChoice/RewordGenerator.ts
+++ src/RewordChoice/RewordGenerator.ts
@@ -8,7 +8,7 @@ export class RewordGenerator {
     private readonly boardSize: BoardSize,
   ) {}
   generate(): Rewords {
-    return [
+    const rewords = [
       {
         name: "normal tile",
         desc: "get 1 normal tile",
@@ -24,6 +24,13 @@ export class RewordGenerator {
         },
       },
       {
+        name: "geta  tile",
+        desc: "get 1 buffer tile. it will plus one to each dice result",
+        callback: () => {
+          this.inventoryModel.addTile("geta", 1);
+        },
+      },
+      {
         name: "upgrade board",
         desc: "board size +1",
         callback: () => {
@@ -31,5 +38,12 @@ export class RewordGenerator {
         },
       },
     ];
+    const select = rewords
+      .keys()
+      .toArray()
+      .sort(() => Math.random() - 0.5)
+      .slice(0, 3)
+      .sort((a, b) => a - b);
+    return [rewords[select[0]], rewords[select[1]], rewords[select[2]]];
   }
 }
