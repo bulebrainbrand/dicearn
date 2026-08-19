8690637a1ccf7d98d368c1724cb4e141b7724e03
bulebrainbrand <h.yotsumoto0509@gmail.com>
2026-08-19 10:47:45 +0900

    refactor: create abstruct tile model

--- src/Tile/GetaTile/Model.ts
+++ src/Tile/GetaTile/Model.ts
@@ -1,31 +1,5 @@
-import EventEmitter from "phaser4-rex-plugins/plugins/utils/eventemitter/EventEmitter.js";
-import { Direction } from "@/Direction.ts";
-import { MovaleTileModel, RotatableTileModel } from "../types";
+import { AbstrustMovableRotetableTile } from "../AbstructTile/Model";
 
-export class GetaTileModel
-  extends EventEmitter
-  implements RotatableTileModel, MovaleTileModel
-{
+export class GetaTileModel extends AbstrustMovableRotetableTile {
   readonly name = "geta";
-  constructor(private dir: Direction) {
-    super();
-  }
-  getDirection(): Direction {
-    return this.dir;
-  }
-  /**
-   * @fires Tile#event:changeDirection
-   * @param dir
-   */
-  changeDirection(dir: Direction) {
-    this.dir = dir;
-    /**
-     * @event Tile#event:changeDirection
-     * @param {Direction} dir
-     */
-    this.emit("changeDirection", dir);
-  }
-  getMovable(): boolean {
-    return true;
-  }
 }
--- src/Tile/MarkTile/Model.ts
+++ src/Tile/MarkTile/Model.ts
@@ -1,31 +1,5 @@
-import EventEmitter from "phaser4-rex-plugins/plugins/utils/eventemitter/EventEmitter.js";
-import { Direction } from "@/Direction.ts";
-import { MovaleTileModel, RotatableTileModel } from "../types";
+import { AbstrustMovableRotetableTile } from "../AbstructTile/Model";
 
-export class MarkTileModel
-  extends EventEmitter
-  implements RotatableTileModel, MovaleTileModel
-{
+export class MarkTileModel extends AbstrustMovableRotetableTile {
   readonly name = "mark";
-  constructor(private dir: Direction) {
-    super();
-  }
-  getDirection(): Direction {
-    return this.dir;
-  }
-  /**
-   * @fires Tile#event:changeDirection
-   * @param dir
-   */
-  changeDirection(dir: Direction) {
-    this.dir = dir;
-    /**
-     * @event Tile#event:changeDirection
-     * @param {Direction} dir
-     */
-    this.emit("changeDirection", dir);
-  }
-  getMovable(): boolean {
-    return true;
-  }
 }
