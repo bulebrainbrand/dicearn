bebc6733e15f09f3e156ff8fcb4c07973e221450
bulebrainbrand <h.yotsumoto0509@gmail.com>
2026-08-21 09:45:44 +0900

    feat: add route searcher logic

--- src/board/RouteSearcher.ts
+++ src/board/RouteSearcher.ts
@@ -31,10 +31,13 @@ export class RouteSearcher {
       x: position.x + offset[0],
       y: position.y + offset[1],
     });
-    if (this.tiles.getTile(destination.x, destination.y) === undefined) {
+    const nextTile = this.tiles.getTile(destination.x, destination.y);
+    if (nextTile === undefined) {
       return { kind: "reset", destination: this.resetPosition };
     }
-
+    if (nextTile.name === "stop") {
+      return { kind: "stop", destination };
+    }
     return { kind: "move", destination };
   }
 
