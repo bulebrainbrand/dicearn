3fed73da5f772eac4db7de6ce95eb16383cf439b
bulebrainbrand <h.yotsumoto0509@gmail.com>
2026-08-28 19:27:45 +0900

    chore: add payment

--- src/Payment/Factory.ts
+++ src/Payment/Factory.ts
@@ -6,8 +6,20 @@ export class PaymentFactory {
   static model(): PaymentModel {
     return new PaymentModel([
       { day: 7, money: 20 },
-      { day: 14, money: 250 },
-      { day: 21, money: 700 },
+      { day: 10, money: 50 },
+      { day: 13, money: 80 },
+      { day: 16, money: 120 },
+      { day: 19, money: 250 },
+      { day: 21, money: 350 },
+      { day: 35, money: 3000 },
+      { day: 42, money: 6000 },
+      { day: 49, money: 9000 },
+      { day: 56, money: 9000 },
+      { day: 63, money: 9000 },
+      { day: 70, money: 18000 },
+      { day: 77, money: 36000 },
+      { day: 84, money: 72000 },
+      { day: 100, money: 250000 },
     ]);
   }
   static create(
